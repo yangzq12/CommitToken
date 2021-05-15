23ea82395a818e741b364cd70235b832cae4a779
yangzq12 <softwareyangzq12@163.com>
2021-05-15 21:28:42 +0800

    feat: implement transferFrom

--- CodeToken.sol
+++ CodeToken.sol
@@ -47,6 +47,9 @@ contract CodeToken is SafeMath {
     }
 
     function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        balances[from] = safeSub(balances[from], tokens);
+        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
         return true;
     }
 }
